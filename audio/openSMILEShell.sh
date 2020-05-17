fde1bbd95cb0200e92fc807dbefbbf9eda24ec7c
Leila Abdelrahman <lxa215@miami.edu>
2020-05-17 15:13:43 -0400

    Vggish audio api
    Co-authored-by: Leila Abdelrahman <leilaabdel@users.noreply.github.com>
    Co-authored-by: Tom Jin <tbj128@users.noreply.github.com>

--- audio/openSMILEShell.sh
+++
@@ -1,6 +0,0 @@
-sudo tar -zxvf opensmile-2.3.0.tar.gz
-sudo sed -i '117s/(char)/(unsigned char)/g' opensmile-2.3.0/src/include/core/vectorTransform.hpp
-sudo apt-get update
-sudo apt-get install autoconf automake libtool m4 gcc
-sudo cd opensmile-2.3.0
-sudo bash buildStandalone.sh
